8f3e3b18a83e9c849a7cfff29ef8d71b4990eedb
Polla <122893119+DearestOne@users.noreply.github.com>
2024-04-03 22:33:32 -0700

    display order ans sebmit order to database
    myshop_order.php can display the order does not serve and can submit order to change in database

--- myshop_order.php
+++ myshop_order.php
@@ -1,3 +1,15 @@
+<?php
+    // header("Refresh: 10");
+    require("connection.php");
+    if(isset($_POST['submit'])){
+        $order_finish = $_POST['serve'];
+        foreach($order_finish as $rows){
+            $u_order = "update orders set served = 1 where order_id = $rows";
+            $conn->query($u_order);
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,24 +30,52 @@
         }
     </style>
 </head>
-<body style="background-image: url(/daismall.png);
+<body style="background-image: url(daismall.png);
     background-attachment: fixed;
     background-repeat: repeat-x;
     background-size: 30ch 20ch;">
-    <img src="/GalaxyDai.PNG" class="center" width="650">
+    <img src="GalaxyDai.PNG" class="center" width="650">
+    <form action="<?php $_PHP_SELF ?>" method="post">
     <table align='center'>
         <tr>
             <td style="font-size: 5ch; text-align: center;">ID</td>
             <td style="font-size: 5ch; text-align: center;">Order</td>
-            <td style="font-size: 5ch; text-align: center;">Total Price</td>
+            <td style="font-size: 5ch; text-align: center;">Quantity</td>
             <td style="font-size: 5ch; text-align: center;">Already</td>
         </tr>
-        <tr>
-            <td style="font-size: 3.5ch; text-align: center;background-color: white;">1</td>
-            <td style="font-size: 3.5ch; text-align: center;background-color: white;">Daifuku</td>
-            <td style="font-size: 3.5ch; text-align: center;background-color: white;">120</td>
-            <td></td>
-        </tr>
+        <?php
+            $s_order_no_serve = "select order_id from orders where served = 0 group by order_id;";
+            $order_no_serve = $conn->query($s_order_no_serve);
+            foreach($order_no_serve as $rows){
+                $current_forder = $rows['order_id'];
+                $s_count = "select count(order_id) as count from order_details where order_id = $current_forder";
+                $count = $conn->query($s_count);
+                $count_detail = array();
+                foreach($count as $rows2){
+                    $count_detail[] = $rows2['count'];
+                }
+                echo "<tr><td rowspan=$count_detail[0] style='font-size: 3.5ch; text-align: center;background-color: white;'>".$current_forder."</td>";
+                $s_datail = "select order_id,m.menu_name,quantity,order_price
+                                from order_details od
+                                join menu m on m.menu_id = od.menu_id
+                                where order_id = $current_forder
+                                order by m.menu_id asc;";
+                $detail = $conn->query($s_datail);
+                $alredy = false;
+                foreach($detail as $rows2){
+                    echo "<td style='font-size: 3.5ch; text-align: center;background-color: white;'>".$rows2['menu_name']."</td>";
+                    echo "<td style='font-size: 3.5ch; text-align: center;background-color: white;'>".$rows2['quantity']."</td>";
+                    if($alredy == false){
+                        echo "<td rowspan=$count_detail[0] style='font-size: 3.5ch; text-align: center;background-color: white;'><input style='width: 40px;
+                        height: 40px;' type='checkbox' name='serve[]' value='$current_forder'></input></td>";
+                        $alredy = true;
+                    }
+                    echo "</tr>";
+                }
+            }
+        ?>
     </table>
+    <input type="submit" name="submit" value="submit"></input>
+    </form>
 </body>
 </html>
