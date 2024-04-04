c723b4c1b30ee8e359992f5ca9639f12e454b6e8
cinnee-pcy <106606412+cinnee-pcy@users.noreply.github.com>
2024-04-04 11:31:59 +0700

    Add files via upload

---
+++ myshop_order.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyShopOrder</title>
+    <link rel="stylesheet" href="design.css">
+    <style>
+        table{
+            border: 0.5ch solid black;
+            width: 70%;
+            font-family: sans-serif;
+            border-collapse: collapse;
+        }
+        th,td{
+            border: 2px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body style="background-image: url(/daismall.png);
+    background-attachment: fixed;
+    background-repeat: repeat-x;
+    background-size: 30ch 20ch;">
+    <img src="/GalaxyDai.PNG" class="center" width="650">
+    <table align='center'>
+        <tr>
+            <td style="font-size: 5ch; text-align: center;">ID</td>
+            <td style="font-size: 5ch; text-align: center;">Order</td>
+            <td style="font-size: 5ch; text-align: center;">Total Price</td>
+            <td style="font-size: 5ch; text-align: center;">Already</td>
+        </tr>
+        <tr>
+            <td style="font-size: 3.5ch; text-align: center;background-color: white;">1</td>
+            <td style="font-size: 3.5ch; text-align: center;background-color: white;">Daifuku</td>
+            <td style="font-size: 3.5ch; text-align: center;background-color: white;">120</td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
